194543f15ed3589cc4479108752c940a97740b4f
tokiedokie <thetokiedokie@gmail.com>
2020-12-01 16:11:00 +0900

    impl getCfg

--- pkg/epr_file.go
+++ pkg/epr_file.go
@@ -1,8 +1,10 @@
 package pkg
 
 import (
+	"bufio"
 	"encoding/binary"
 	"os"
+	"strings"
 )
 
 type eprFileMethod interface {
@@ -12,7 +14,8 @@ type eprFileMethod interface {
 }
 
 type eprFile struct {
-	filePath string
+	dataPath string
+	cfgPath string
 }
 
 type eprFileInt8 struct {
@@ -23,38 +26,57 @@ type eprFileInt32 struct {
 	eprFile
 }
 
-func newEprFileInt8(filePath string) *eprFileInt8 {
+func newEprFileInt8(dataPath string) *eprFileInt8 {
 	f := new(eprFileInt8)
-	f.filePath = filePath
+	f.dataPath = dataPath
 	return f
 }
 
-func newEprFileInt32(filePath string) *eprFileInt32 {
+func newEprFileInt32(dataPath string, cfgPath string) *eprFileInt32 {
 	f := new(eprFileInt32)
-	f.filePath = filePath
+	f.dataPath = dataPath
+	f.cfgPath = cfgPath
 	return f
 }
 
 func (e *eprFileInt8) getData() []int8 {
 	bufSize := e.dataSize()/1 // dividing by 4 because int8 has 1 byte.
 	data := make([]int8, bufSize)
-	readFile(e.filePath, binary.BigEndian, &data)
+	readFile(e.dataPath, binary.BigEndian, &data)
 	return data
 }
 
 func (e *eprFileInt32) getData() []int32 {
 	bufSize := e.dataSize()/4 // dividing by 4 because int8 has 4 byte.
 	data := make([]int32, bufSize)
-	readFile(e.filePath, binary.BigEndian, &data)
+	readFile(e.dataPath, binary.BigEndian, &data)
 	return data
 }
 
-func (e *eprFile) getCfg() {
-	// cfgMap := make(map[string]string)
+func (e *eprFile) getCfg() map[string]string {
+	cfgFile, _ := os.Open(e.cfgPath)
+	defer cfgFile.Close()
+	
+	scanner := bufio.NewScanner(cfgFile)
+
+	cfgMap := make(map[string]string)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.HasPrefix(text, "#") || strings.HasPrefix(text, "*") { continue }
+		kv := strings.Fields(text)
+		if len(kv) < 2 {
+			cfgMap[kv[0]] = ""	
+			continue
+		}
+		cfgMap[kv[0]] = kv[1]
+	}
+	
+	return cfgMap
 }
 
 func (e *eprFile) dataSize() int64 {
-	info, err := os.Stat(e.filePath)
+	info, err := os.Stat(e.dataPath)
 	if err != nil {
 		panic(err)
 	}
