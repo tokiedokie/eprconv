a7470aff57fc9d8dccd9113c74e1d9c239dbe453
tokiedokie <thetokiedokie@gmail.com>
2020-12-02 16:16:45 +0900

    add getDataCfgPath

--- pkg/epr_load.go
+++ pkg/epr_load.go
@@ -1,20 +1,45 @@
 package pkg
 
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
 type fileFormat int
 
 const (
 	brukerBES3T fileFormat = iota + 1
 )
 
-func EprLoad(filePath string) {
-	switch asumeFormat(filePath) {
-	case brukerBES3T:
-		loadBrukerBES3T(filePath)
-	default:
-		panic("cannot load a epr file")
+func asumeFormat(filePath string) fileFormat {
+	switch strings.ToLower(filepath.Ext(filePath)) {
+	case ".dta", ".dsc":
+		return brukerBES3T
 	}
+	panic("format is not supported")
 }
 
-func loadBrukerBES3T(filePath string) {
-	// fint8 := newEprFileInt8(filePath)
+func EprLoad(filePath string) {
+}
+
+func getDataCfgPath(filePath string) (dataPath, cfgPath string) {
+	if filepath.Ext(filePath) == "" {
+		dir, _ := filepath.Split(filePath)
+		fileInfos, _ := ioutil.ReadDir(dir)
+		for _, fileInfo := range fileInfos {
+			fmt.Println(fileInfo.Name())
+		}
+	
+		// get file path with an extention
+		// if no files found, then throw error
+	}
+	switch asumeFormat(filePath) {
+
+	}
+
+
+
+	return
 }
--- pkg/epr_load_test.go
+++ pkg/epr_load_test.go
@@ -2,7 +2,6 @@ package pkg
 
 import "testing"
 
-func TestLoadBrukerBES3T(t *testing.T) {
-	path := "../test/data/bes3tint.dta"
-	loadBrukerBES3T(path)
+func TestGetDataCfgPath(t *testing.T) {
+	getDataCfgPath("../test")
 }
