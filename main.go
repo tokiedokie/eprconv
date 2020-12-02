bea2573c844b5dd292ea6d931ebec2207ee2188d
tokiedokie <thetokiedokie@gmail.com>
2020-12-02 16:17:22 +0900

    add command line interface

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"eprconv/pkg"
+	"flag"
+)
+
 func main() {
+	var (
+		c = flag.String("c","", "path to config file")
+		d = flag.String("d","", "path to data file")
+		o = flag.String("o","", "path to output file")
+	)
+	flag.Parse()
+	
+	epr := pkg.NewEprFile(*d, *c)
+	pkg.Output(*o, *epr)
 }
