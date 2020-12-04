bd2784950ea8f6f34e6e6895406d9879dc75246e
tokiedokie <thetokiedokie@gmail.com>
2020-12-04 10:34:20 +0900

    update TestGetDataCfgPath

--- pkg/epr_load_test.go
+++ pkg/epr_load_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestGetDataCfgPath(t *testing.T) {
-	dp, cp, _ := getDataCfgPath("../test/data/bes3tint")
+	dp, cp, _ := getDataCfgPath("../test/data/bes3tint.dta")
+	assert.Equal(t, dp, "../test/data/bes3tint.dta")
+	assert.Equal(t, cp, "../test/data/bes3tint.dsc")
+	dp, cp, _ = getDataCfgPath("../test/data/bes3tint")
 	assert.Equal(t, dp, "../test/data/bes3tint.dta")
 	assert.Equal(t, cp, "../test/data/bes3tint.dsc")
 }
